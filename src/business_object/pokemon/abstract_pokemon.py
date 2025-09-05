dc2ba52ea0cefbb2910f1558801dfa3c6ffe1235
mathiskacer2 <mathis.kacer@eleve.ensai.fr>
2025-09-05 08:42:13 +0000

    premieres étapes

--- src/business_object/pokemon/abstract_pokemon.py
+++ src/business_object/pokemon/abstract_pokemon.py
@@ -1,9 +1,9 @@
 import copy
 
 from business_object.statistic import Statistic
+from abc import ABC, abstractmethod
 
-
-class Pokemon:
+class Pokemon(ABC):
     """
     A Pokemon
     """
@@ -26,6 +26,7 @@ class Pokemon:
     # Methods
     # -------------------------------------------------------------------------
 
+    @abstractmethod
     def get_pokemon_attack_coef(self) -> float:
         """
         Compute a damage multiplier related to the pokemon type.
