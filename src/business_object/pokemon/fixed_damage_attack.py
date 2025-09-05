a85586ea58cdf65e753764337dc4ccc87817c6e5
mathiskacer2 <mathis.kacer@eleve.ensai.fr>
2025-09-05 12:04:50 +0000

    encore

--- src/business_object/pokemon/fixed_damage_attack.py
+++ src/business_object/pokemon/fixed_damage_attack.py
@@ -3,4 +3,3 @@ from business_object.pokemon.abstract_attack import AbstractAttack
 class FixedDamageAttack(AbstractAttack):
     def compute_damage():
         return self._power
-
--- src/tests/test_business_object/test_pokemon/test_fixed_damage_attack.py
+++ src/tests/test_business_object/test_pokemon/test_fixed_damage_attack.py
@@ -3,3 +3,4 @@ from business_object.pokemon.fixed_damage_attack import FixedDamageAttack
 class TestFixedDamageAttack:
     def test_compute_damage(self):
         pass
+    
